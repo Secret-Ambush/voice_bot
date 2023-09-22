61f4af915c766ddd9fdcdf663ef3c9fa20d8110e
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2023-09-22 08:49:05 +0400

    Update voice_control.py

--- catkin_ws/src/voice_controlled_bot/src/voice_control.py
+++ catkin_ws/src/voice_controlled_bot/src/voice_control.py
@@ -6,8 +6,8 @@ import speech_recognition as sr
 from geometry_msgs.msg import Twist
 
 motion_command = Twist()
-motion_publisher = None  # Initialize the motion_publisher
-text_publisher = None  # Initialize the text_publisher
+motion_publisher = None  
+text_publisher = None  
 
 def speech_to_text_callback(event):
     global text_publisher  # Declare text_publisher as global
@@ -16,13 +16,12 @@ def speech_to_text_callback(event):
 
     try:
         with sr.Microphone() as source:
-            print("Listening: ")
-            audio = r.listen(source, timeout = 2) #2 seconds timeout
-            print("Processing: ")
+	    print("Listening now: ")
+            audio = r.listen(source, timeout = 1)
+            print("Stopped Listening")
             text = r.recognize_google(audio)
-            print(text)
             rospy.loginfo("Recognized text: %s", text)
-            text_publisher.publish(text)  # Publish the recognized text
+            text_publisher.publish(text)  # Publishing the recognized text
 
     except sr.UnknownValueError:
         rospy.logwarn("Could not recognize speech")
@@ -30,34 +29,35 @@ def speech_to_text_callback(event):
     except sr.RequestError as e:
         rospy.logerr("Speech recognition error: %s", e)
         
-    # rospy.sleep(2)  # Sleep for 2 seconds
+    # rospy.sleep(2)  
 
 def process_voice_command(text_msg):
     global motion_command, motion_publisher
+
     text = text_msg.data
 
     if "left" in text:
         rospy.loginfo("Command: Left")
-        # Generate motion command for turning left
+        
         motion_command.linear.x = 0.0
-        motion_command.angular.z = 0.2  # Adjust the angular velocity as needed
-        # Publish the motion command
+        motion_command.angular.z = 0.2  
+        # Publishing movement commands
         motion_publisher.publish(motion_command)
 
     elif "right" in text:
         rospy.loginfo("Command: Right")
-        # Generate motion command for turning right
+       
+        motion_command.angular.z = -0.2  
         motion_command.linear.x = 0.0
-        motion_command.angular.z = -0.2  # Adjust the angular velocity as needed
-        # Publish the motion command
+        # Publishing movement commands
         motion_publisher.publish(motion_command)
 
     elif "straight" in text:
         rospy.loginfo("Command: Straight")
-        # Generate motion command for moving straight
-        motion_command.linear.x = 0.2  # Adjust the linear velocity as needed
+        
+        motion_command.linear.x = 0.2  
         motion_command.angular.z = 0.0
-        # Publish the motion command
+        # Publishing movement commands
         motion_publisher.publish(motion_command)
 
     else:
@@ -68,15 +68,16 @@ def stop_robot():
     global motion_command, motion_publisher
     motion_command.linear.x = 0.0
     motion_command.angular.z = 0.0
-    motion_publisher.publish(motion_command)  # Publish the stop command
+    motion_publisher.publish(motion_command)  # Publishing stop command
 
 if __name__ == '__main__':
-    rospy.init_node('voice_commands', anonymous=True)
-    text_publisher = rospy.Publisher( '/recognized_text', String, queue_size=10)  # Publish recognized text
+    rospy.init_node('voice_commands2', anonymous=True)
+    text_publisher = rospy.Publisher(
+        '/recognized_text', String, queue_size=1)  # Publishing text
     
-    # Create a timer that calls speech_to_text_callback every 2 seconds
+    # Creating a timer 
     timer = rospy.Timer(rospy.Duration(2), speech_to_text_callback)
     
     text_subscriber = rospy.Subscriber('/recognized_text',String, process_voice_command)
-    motion_publisher = rospy.Publisher( '/cmd_vel', Twist, queue_size=10)  # Publish robot motion commands
+    motion_publisher = rospy.Publisher( '/cmd_vel', Twist, queue_size=1)  # Publishing movement commands
     rospy.spin()
