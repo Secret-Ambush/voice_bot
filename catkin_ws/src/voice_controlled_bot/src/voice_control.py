068350d5c2c29fe746042e8a942458c42b7b5772
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2023-09-26 10:08:10 +0400

    Updated digit mapping

--- catkin_ws/src/voice_controlled_bot/src/voice_control.py
+++ catkin_ws/src/voice_controlled_bot/src/voice_control.py
@@ -42,10 +42,14 @@ def process_voice_command(text_msg):
 
         # Map the extracted text to numeric values
     digit_mapping = {
-            "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-            "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10,
-            "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10
-        }
+    "1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+    "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+    "11": 11, "12": 12, "13": 13, "14": 14, "15": 15,
+    "16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
+    "21": 21, "22": 22, "23": 23, "24": 24, "25": 25,
+    "26": 26, "27": 27, "28": 28, "29": 29, "30": 30
+}
+
 
     if digit_text in digit_mapping:
         digit = digit_mapping[digit_text]
