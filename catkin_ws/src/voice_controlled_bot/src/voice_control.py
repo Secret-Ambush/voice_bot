816320a8e8b665d0e0b3b5cd17778bfb4bbfdbed
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2023-10-09 11:21:21 +0400

    Update voice_control.py

--- catkin_ws/src/voice_controlled_bot/src/voice_control.py
+++ catkin_ws/src/voice_controlled_bot/src/voice_control.py
@@ -19,17 +19,17 @@ def speech_to_text_callback(event):
     try:
         with sr.Microphone() as source:
 	    print("Listening now: ")
-            audio = r.listen(source, timeout = 5)
+            audio = r.listen(source, timeout = 2)
             print("Stopped Listening")
-            text = r.recognize_google(audio)
-            print("Recognized text: %s", text)
+            text = r.recognize_google(audio, show_all = True)
+            print("Recognized text: ", text)
             text_publisher.publish(text)  # Publishing the recognized text
 
     except sr.UnknownValueError:
         rospy.logwarn("Could not recognize speech")
 
     except sr.RequestError as e:
-        rospy.logerr("Speech recognition error: %s", e)
+        rospy.logerr("Speech recognition error: ", e)
         
     # rospy.sleep(2)  
 
@@ -38,31 +38,33 @@ def process_voice_command(text_msg):
 
     text = text_msg.data
     digit_match = re.search(r'\b([1-9]|10|1[1-9]|20|30)\b', text)
-    digit_text = digit_match.group(0)
 
-        # Map the extracted text to numeric values
-    digit_mapping = {
-    "1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
-    "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
-    "11": 11, "12": 12, "13": 13, "14": 14, "15": 15,
-    "16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
-    "21": 21, "22": 22, "23": 23, "24": 24, "25": 25,
-    "26": 26, "27": 27, "28": 28, "29": 29, "30": 30
-}
+    if digit_match:
+		digit_text = digit_match.group(0)
 
-    if digit_text in digit_mapping:
-        digit = digit_mapping[digit_text]
-        rospy.loginfo("Recognized digit: %s", digit)
-
-    if digit_text in digit_mapping:  
-		linear_value = float(digit) / 10.0
+		# Map the extracted text to numeric values
+		digit_mapping = {
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+		"6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+		"11": 11, "12": 12, "13": 13, "14": 14, "15": 15,
+		"16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
+		"21": 21, "22": 22, "23": 23, "24": 24, "25": 25,
+		"26": 26, "27": 27, "28": 28, "29": 29, "30": 30
+		}
+		
+		if digit_text in digit_mapping:
+			digit = digit_mapping[digit_text]
+			linear_value = float(digit) / 10.0
+			rospy.loginfo("Recognized digit: ", digit)
+		
     else:
         linear_value = 0.0
+            
 
     if "left" in text:
         rospy.loginfo("Command: Left")
         motion_command.angular.z = 0.2
-		motion_command.linear.x = linear_value
+	motion_command.linear.x = linear_value
 		
         # Publishing movement commands
         motion_publisher.publish(motion_command)
@@ -83,6 +85,10 @@ def process_voice_command(text_msg):
         # Publishing movement commands
         motion_publisher.publish(motion_command)
 
+    elif "stop" in text:
+        rospy.loginfo("Command: Stop") 
+        stop_robot()
+
     else:
         rospy.loginfo("Unrecognized command")
         stop_robot()  # Stop the robot for unrecognized commands
@@ -102,6 +108,7 @@ if __name__ == '__main__':
     units = "units"
     command = ("move" direction "by" distance units | "turn" direction | "stop" | "move" direction )
 	"""
+
     rospy.init_node('voice_commands2', anonymous=True)
     text_publisher = rospy.Publisher(
         '/recognized_text', String, queue_size=1)  # Publishing text
