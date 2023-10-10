83bd256e853fd6f748decd2d1005561df23e84da
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2023-10-10 09:12:43 +0400

    indentation

--- catkin_ws/src/voice_controlled_bot/src/voice_control.py
+++ catkin_ws/src/voice_controlled_bot/src/voice_control.py
@@ -18,24 +18,24 @@ def speech_to_text_callback(event):
 	
 	try:
 		with sr.Microphone() as source:
-		print("Listening now: ")
-		audio = r.listen(source, timeout=2)
-		print("Stopped Listening")
-		text = r.recognize_google(audio, show_all=True)
+			print("Listening now: ")
+			audio = r.listen(source, timeout=2)
+			print("Stopped Listening")
+			text = r.recognize_google(audio, show_all=True)
 		
 		alternative_list = text.get('alternative', [])
 		
 		# Iterating to find text with numeric digits
 		selected_text = ""
 		for item in alternative_list:
-		transcript = item.get('transcript', '')
-		if any(char.isdigit() for char in transcript):
-		selected_text = transcript
-		break
+			transcript = item.get('transcript', '')
+			if any(char.isdigit() for char in transcript):
+				selected_text = transcript
+				break
 		
 		# If no text with numeric digits found, select the first one
 		if selected_text is None and alternative_list:
-		selected_text = alternative_list[0].get('transcript', '')
+			selected_text = alternative_list[0].get('transcript', '')
 		
 		print("Selected Text:", selected_text)
 		
