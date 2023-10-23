20797ad38dcff795d2b95ae6b01b95bedfe4dc50
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2023-10-23 10:52:23 +0400

    Update voice_control.py

--- catkin_ws/src/voice_controlled_bot/src/voice_control.py
+++ catkin_ws/src/voice_controlled_bot/src/voice_control.py
@@ -29,14 +29,14 @@ def speech_to_text_callback(event):
 		# Iterating to find text with numeric digits
 		selected_text = ""
 		for item in alternative_list:
-		    transcript = item.get('transcript', '')
-		    if any(char.isdigit() for char in transcript):
-		        selected_text = transcript
-		        break
+			transcript = item.get('transcript', '')
+			if any(char.isdigit() for char in transcript):
+				selected_text = transcript
+				break
 		
 		# If no text with numeric digits found, select the first one
-		if not selected_text and alternative_list:
-		    selected_text = alternative_list[0].get('transcript', '')
+		if selected_text is None and alternative_list:
+			selected_text = alternative_list[0].get('transcript', '')
 		
 		print("Selected Text:", selected_text)
 		
@@ -44,11 +44,9 @@ def speech_to_text_callback(event):
 	
 	except sr.UnknownValueError:
 		rospy.logwarn("Could not recognize speech")
-		stop_robot()
 	
 	except Exception as e:
-		rospy.logerr(f"Speech recognition error: {str(e)}")
-		stop_robot()
+		rospy.logerr("Speech recognition error: ", e)
 	
 	# rospy.sleep(2)  
 
@@ -58,64 +56,45 @@ def process_voice_command(text_msg):
 	text = text_msg.data
 	digit_match = re.search(r'\b([1-9]|10|1[1-9]|20|30)\b', text)
 	
-	#digit mapping approach (tedious)
-	'''
-	if digit_match:
-	digit_text = digit_match.group(0)
-	
-	# Map the extracted text to numeric values
-	digit_mapping = {
-	    "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-	    "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10,
-	    "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15,
-	    "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20,
-	    "twenty-one": 21, "twenty-two": 22, "twenty-three": 23, "twenty-four": 24, "twenty-five": 25,
-	    "twenty-six": 26, "twenty-seven": 27, "twenty-eight": 28, "twenty-nine": 29, "thirty": 30
-	}
- 
-	'''
-	
+	# digit mapping approach (tedious)
 	if digit_match:
 		digit = int(digit_match.group(0))
 		linear_value = float(digit) / 10.0
-		rospy.loginfo("Recognized digit: ", digit)
+		rospy.loginfo("Recognized digit: %s", digit)
 	else:
 		linear_value = 0.0
-
-	print("Linear Value: ", linear_value)
-	
 	
 	if "left" in text:
 		rospy.loginfo("Command: Left")
 		motion_command.angular.z = 0.2
 		motion_command.linear.x = linear_value
-		
-		# Publishing movement commands
 		motion_publisher.publish(motion_command)
 	
 	elif "right" in text:
 		rospy.loginfo("Command: Right")
 		motion_command.angular.z = -0.2
 		motion_command.linear.x = linear_value
-		
-		# Publishing movement commands
 		motion_publisher.publish(motion_command)
 	
 	elif "straight" in text:
-		rospy.loginfo("Command: Straight") 
+		rospy.loginfo("Command: Straight")
 		motion_command.angular.z = 0.0
 		motion_command.linear.x = linear_value
-		
-		# Publishing movement commands
 		motion_publisher.publish(motion_command)
 	
 	elif "stop" in text:
-		rospy.loginfo("Command: Stop") 
+		rospy.loginfo("Command: Stop")
 		stop_robot()
 	
 	else:
 		rospy.loginfo("Unrecognized command")
 		stop_robot()  # Stop the robot for unrecognized commands
+	
+	# Additional check for command not matching the grammar
+	if not re.match(text_grammar, text):
+		rospy.logerr("Command does not match the grammar: ", text)
+		stop_robot()
+
 
 def stop_robot():
 	global motion_command, motion_publisher
@@ -130,15 +109,14 @@ if __name__ == '__main__':
 	direction = "left" | "right" | "straight"
 	distance = /([1-9]|10|1[1-9]|20|30)/  # Matches numbers from 1 to 30
 	units = "units"
-	command = ("move" direction "by" distance units | "turn" direction | "stop" | "move" direction )
+	command = ("move" direction "by" distance units | "turn" direction | "move" direction | "stop" | "move" direction )
 	"""
 	
 	rospy.init_node('voice_commands2', anonymous=True)
-	text_publisher = rospy.Publisher(
-	'/recognized_text', String, queue_size=1)  # Publishing text
+	text_publisher = rospy.Publisher( '/recognized_text', String, queue_size=1)  # Publishing text
 	
 	# Creating a timer 
-	timer = rospy.Timer(rospy.Duration(2), speech_to_text_callback)
+	timer = rospy.Timer(rospy.Duration(5), speech_to_text_callback)
 	
 	text_subscriber = rospy.Subscriber('/recognized_text',String, process_voice_command)
 	motion_publisher = rospy.Publisher( '/cmd_vel', Twist, queue_size=1)  # Publishing movement commands
